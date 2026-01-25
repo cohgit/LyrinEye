0bf17c93c0f8bdb3b8757b902b91c7218fc4aa7f
Cesar Ogalde H. <cesar.ogalde@gmail.com>
2026-01-25 20:20:09 -0300

    feat(mobile): sync apk version with release tag and update backend url to stable endpoint

--- mobile/src/config.ts
+++ mobile/src/config.ts
@@ -1,4 +1,4 @@
 export const CONFIG = {
     // Use https:// because Azure Container Apps handles SSL
-    SIGNALING_SERVER: 'https://lyrineye-backend--o568gsy.icymoss-5b66c974.eastus.azurecontainerapps.io',
+    SIGNALING_SERVER: 'https://lyrineye-backend.icymoss-5b66c974.eastus.azurecontainerapps.io',
 };
