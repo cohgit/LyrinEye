486ef189ff0a300d86f87c3cda1516e7aaa879cf
Cesar Ogalde H. <cesar.ogalde@gmail.com>
2026-01-27 23:14:44 -0300

    fix(mobile): resolve CMake geolocation error and modernize library build scripts

---
+++ mobile/react-native.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dependencies: {
+        '@react-native-community/geolocation': {
+            platforms: {
+                android: {
+                    libraryName: null,
+                    componentName: null,
+                },
+            },
+        },
+    },
+};
