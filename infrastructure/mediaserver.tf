a30c75d46ca73a50bb20fa85b32b67e4db143473
Cesar Ogalde H. <cesar.ogalde@gmail.com>
2026-02-05 00:40:39 -0300

    feat: add Azure DNS label for default FQDN

--- infrastructure/mediaserver.tf
+++ infrastructure/mediaserver.tf
@@ -28,6 +28,7 @@ resource "azurerm_public_ip" "mediasoup" {
   resource_group_name = azurerm_resource_group.main.name
   allocation_method   = "Static"
   sku                 = "Standard"
+  domain_name_label   = "lyrineye-stream"
   
   tags = {
     Environment = var.environment
